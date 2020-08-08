9207fa5e572aef4c88fde349864b06e6a1c17e23
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-08-08 09:49:03 -0400

    Create crates

---
+++ swift/src/lib.rs
@@ -0,0 +1,5 @@
+//! This is a work-in-progress by [Nikolai Vazquez](https://twitter.com/nikolaivazquez).
+//! If you would like to get involved,
+//! <a href="mailto:hello@nikolaivazquez.com?subject=I want to help with swift-bindgen&body=Hi Nikolai,%0A%0AMy name is YOUR NAME and I want to get involved with swift-bindgen by...">reach out</a>!
+
+#![warn(missing_docs)]
