c29c409f4f9efd6c9fd2173137933e8dbbe4a199
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-11-19 18:49:32 -0400

    Use intra-doc links for `Metadata` method docs
    These links point to the fields of `ValueWitnessTable`.

--- swift-rt/src/metadata/metadata.rs
+++ swift-rt/src/metadata/metadata.rs
@@ -206,7 +206,7 @@ impl Metadata {
 /// because they pass the expected metadata pointer to the `self` parameter.
 impl Metadata {
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.initialize_buffer_with_copy_of_buffer).
+    /// [the value-witness function](ValueWitnessTable#structfield.initialize_buffer_with_copy_of_buffer).
     #[inline(always)]
     pub unsafe fn vw_initialize_buffer_with_copy_of_buffer<T, B>(
         &self,
@@ -221,42 +221,42 @@ impl Metadata {
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.destroy).
+    /// [the value-witness function](ValueWitnessTable#structfield.destroy).
     #[inline(always)]
     pub unsafe fn vw_destroy<T>(&self, object: *mut T) {
         self.value_witnesses().destroy(object, self);
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.initialize_with_copy).
+    /// [the value-witness function](ValueWitnessTable#structfield.initialize_with_copy).
     #[inline(always)]
     pub unsafe fn vw_initialize_with_copy<T>(&self, dest: *mut T, src: *mut T) -> *mut T {
         self.value_witnesses().initialize_with_copy(dest, src, self)
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.assign_with_copy).
+    /// [the value-witness function](ValueWitnessTable#structfield.assign_with_copy).
     #[inline(always)]
     pub unsafe fn vw_assign_with_copy<T>(&self, dest: *mut T, src: *mut T) -> *mut T {
         self.value_witnesses().assign_with_copy(dest, src, self)
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.initialize_with_take).
+    /// [the value-witness function](ValueWitnessTable#structfield.initialize_with_take).
     #[inline(always)]
     pub unsafe fn vw_initialize_with_take<T>(&self, dest: *mut T, src: *mut T) -> *mut T {
         self.value_witnesses().initialize_with_take(dest, src, self)
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.assign_with_take).
+    /// [the value-witness function](ValueWitnessTable#structfield.assign_with_take).
     #[inline(always)]
     pub unsafe fn vw_assign_with_take<T>(&self, dest: *mut T, src: *mut T) -> *mut T {
         self.value_witnesses().assign_with_take(dest, src, self)
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.get_enum_tag_single_payload).
+    /// [the value-witness function](ValueWitnessTable#structfield.get_enum_tag_single_payload).
     #[inline(always)]
     pub unsafe fn vw_get_enum_tag_single_payload<T>(
         &self,
@@ -268,7 +268,7 @@ impl Metadata {
     }
 
     /// A generic wrapper over
-    /// [the value-witness function](struct.ValueWitnessTable.html#structfield.store_enum_tag_single_payload).
+    /// [the value-witness function](ValueWitnessTable#structfield.store_enum_tag_single_payload).
     #[inline(always)]
     pub unsafe fn vw_store_enum_tag_single_payload<T>(
         &self,
