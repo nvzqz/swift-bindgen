012e8f5cac4a2f54d1d2e0314c2cf19525491de4
Nikolai Vazquez <hello@nikolaivazquez.com>
2021-07-26 19:38:16 -0400

    Create methods for comparing `String` ordering

--- swift/src/string/cmp.rs
+++ swift/src/string/cmp.rs
@@ -1,38 +1,92 @@
-use crate::{Equatable, String};
-use std::{mem::ManuallyDrop, ptr};
+use crate::{Comparable, Equatable, String};
+use std::{cmp::Ordering, mem::ManuallyDrop, ptr};
 
-// SAFETY: String implements Equatable.
+// SAFETY: String implements Equatable and Comparable.
 //
-// TODO: In the future, get conformance descriptor at `$sSSSQsMc`.
+// TODO: Get conformance descriptors at `$sSSSQsMc` and `$sSSSLsMc`.
 unsafe impl Equatable for String {}
+unsafe impl Comparable for String {}
+
+// TODO: Implement these inline rather than only rely the stdlib funcs.
+mod funcs {
+    use super::*;
+
+    // SAFETY: Strings are passed by-value using the C calling convention for
+    // structs. The called function takes no ownership responsibility, so we
+    // denote this with `ManuallyDrop`.
+    //
+    // On x86_64, this should pass:
+    // - `%rdi` and `%rsi` from `lhs`
+    // - `%rdx` and `%rcx` from `rhs`
+    #[link(name = "swiftCore", kind = "dylib")]
+    extern "C" {
+        // Only `==` and `<` infix funcs are available. All other operations are
+        // implemented in terms of these.
+
+        #[link_name = "$sSS2eeoiySbSS_SStFZ"]
+        pub fn eq(lhs: ManuallyDrop<String>, rhs: ManuallyDrop<String>) -> bool;
+
+        #[link_name = "$sSS1loiySbSS_SStFZ"]
+        pub fn lt(lhs: ManuallyDrop<String>, rhs: ManuallyDrop<String>) -> bool;
+    }
+}
 
 impl PartialEq for String {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
-        // TODO: Implement this inline rather than only rely the stdlib func.
-
-        // SAFETY: Strings are passed by-value using the C calling convention
-        // for structs. The called function takes no ownership responsibility,
-        // so we denote this with `ManuallyDrop`.
-        //
-        // On x86_64, this should pass:
-        // - `%rdi` and `%rsi` from `self`
-        // - `%rdx` and `%rcx` from `other`
         unsafe {
-            #[link(name = "swiftCore", kind = "dylib")]
-            extern "C" {
-                #[link_name = "$sSS2eeoiySbSS_SStFZ"]
-                fn eq(a: ManuallyDrop<String>, b: ManuallyDrop<String>) -> bool;
-            }
-
-            let a = ManuallyDrop::new(ptr::read(self));
-            let b = ManuallyDrop::new(ptr::read(other));
-            eq(a, b)
+            let lhs = ManuallyDrop::new(ptr::read(self));
+            let rhs = ManuallyDrop::new(ptr::read(other));
+            funcs::eq(lhs, rhs)
         }
     }
 }
 
 impl Eq for String {}
+
+impl PartialOrd for String {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+
+    #[inline]
+    fn lt(&self, other: &Self) -> bool {
+        unsafe {
+            let lhs = ManuallyDrop::new(ptr::read(self));
+            let rhs = ManuallyDrop::new(ptr::read(other));
+            funcs::lt(lhs, rhs)
+        }
+    }
+
+    #[inline]
+    fn le(&self, other: &Self) -> bool {
+        !(other < self)
+    }
+
+    #[inline]
+    fn gt(&self, other: &Self) -> bool {
+        other > self
+    }
+
+    #[inline]
+    fn ge(&self, other: &Self) -> bool {
+        !(self < other)
+    }
+}
+
+impl Ord for String {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self < other {
+            Ordering::Less
+        } else if self == other {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -41,5 +95,7 @@ mod tests {
     #[test]
     fn eq_new() {
         assert!(String::new() == String::new());
+        assert!(String::new() <= String::new());
+        assert!(String::new() >= String::new());
     }
 }
